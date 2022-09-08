62c44e7ec909eee27f6ad5c77559fbefec9c515a
Serge Bayet <serge.bayet1975@gmail.com>
2022-09-08 08:10:28 +0200

    change ns -> nameserver

--- Protocols/EPP/eppExtensions/domain-ext-2.3/eppResponses/euridEppInfoDomainResponse.php
+++ Protocols/EPP/eppExtensions/domain-ext-2.3/eppResponses/euridEppInfoDomainResponse.php
@@ -16,108 +16,149 @@ namespace Metaregistrar\EPP;
  */
 
 class euridEppInfoDomainResponse extends eppInfoDomainResponse {
-    function __construct() {
-        parent::__construct();
-    }
+	function __construct() {
+		parent::__construct();
+	}
+  /**
+	 *
+	 * @return boolean|null
+	 */
+	public function getNameserverGroup() {
+    $result = $this->getElementsByTagNameNS('http://www.eurid.eu/xml/epp/domain-ext-2.3', 'nsgroup');
+		if ($result->length > 0) {
+			return $result->item(0)->nodeValue;
+		} else {
+			return null;
+		}
+	}
+/**
+	 *
+	 * @return boolean|null
+	 */
+	public function getMaxExtensionPeriod() {
+    $result = $this->getElementsByTagNameNS('http://www.eurid.eu/xml/epp/domain-ext-2.3', 'maxExtensionPeriod');
+		if ($result->length > 0) {
+			return (int) $result->item(0)->nodeValue;
+		} else {
+			return null;
+		}
+	}
+  /**
+	 *
+	 * @return boolean|null
+	 */
+	public function getRegistrantCountry() {
+    $result = $this->getElementsByTagNameNS('http://www.eurid.eu/xml/epp/domain-ext-2.3', 'registrantCountry');
+		if ($result->length > 0) {
+			return $result->item(0)->nodeValue;
+		} else {
+			return null;
+		}
+	}
 
-    /**
-     *
-     * @return boolean|null
-     */
-    public function getQuarantined() {
-        $xpath = $this->xPath();
-        $result = $xpath->query('/epp:epp/epp:response/epp:extension/eurid:ext/eurid:infData/eurid:domain/eurid:quarantined');
-        if ($result->length > 0) {
-            if ($result->item(0)->nodeValue == 'true') {
-                return true;
-            } else {
-                return false;
-            }
-        } else {
-            return null;
-        }
-    }
+  /**
+	 *
+	 * @return boolean|null
+	 */
+	public function getDelayed() {
+    $result = $this->getElementsByTagNameNS('http://www.eurid.eu/xml/epp/domain-ext-2.3', 'delayed');
+		if ($result->length > 0) {
+			if ($result->item(0)->nodeValue == 'true') {
+				return true;
+			} else {
+				return false;
+			}
+		} else {
+			return null;
+		}
+	}
 
+	/**
+	 *
+	 * @return boolean|null
+	 */
+	public function getQuarantined() {
+    $result = $this->getElementsByTagNameNS('http://www.eurid.eu/xml/epp/domain-ext-2.3', 'quarantined');
+		if ($result->length > 0) {
+			if ($result->item(0)->nodeValue == 'true') {
+				return true;
+			} else {
+				return false;
+			}
+		} else {
+			return null;
+		}
+	}
 
-    /**
-     *
-     * @return boolean|null
-     */
-    public function getOnHold() {
-        $xpath = $this->xPath();
-        $result = $xpath->query('/epp:epp/epp:response/epp:extension/eurid:ext/eurid:infData/eurid:domain/eurid:onhold');
-        if ($result->length > 0) {
-            if ($result->item(0)->nodeValue == 'true') {
-                return true;
-            } else {
-                return false;
-            }
-        } else {
-            return null;
-        }
-    }
+	/**
+	 *
+	 * @return boolean|null
+	 */
+	public function getOnHold() {
+		$result = $this->getElementsByTagNameNS('http://www.eurid.eu/xml/epp/domain-ext-2.3', 'onHold');
+		if ($result->length > 0) {
+			if ($result->item(0)->nodeValue == 'true') {
+				return true;
+			} else {
+				return false;
+			}
+		} else {
+			return null;
+		}
+	}
 
-    /**
-     *
-     * @return boolean|null
-     */
-    public function getSuspended() {
-        $xpath = $this->xPath();
-        $result = $xpath->query('/epp:epp/epp:response/epp:extension/eurid:ext/eurid:infData/eurid:domain/eurid:suspended');
-        if ($result->length > 0) {
-            if ($result->item(0)->nodeValue == 'true') {
-                return true;
-            } else {
-                return false;
-            }
-        } else {
-            return null;
-        }
-    }
+	/**
+	 *
+	 * @return boolean|null
+	 */
+	public function getSuspended() {
+    $result = $this->getElementsByTagNameNS('http://www.eurid.eu/xml/epp/domain-ext-2.3', 'suspended');
+		if ($result->length > 0) {
+			if ($result->item(0)->nodeValue == 'true') {
+				return true;
+			} else {
+				return false;
+			}
+		} else {
+			return null;
+		}
+	}
 
-    /**
-     *
-     * @return boolean|null
-     */
-    public function getSeized() {
-        $xpath = $this->xPath();
-        $result = $xpath->query('/epp:epp/epp:response/epp:extension/eurid:ext/eurid:infData/eurid:domain/eurid:seized');
-        if ($result->length > 0) {
-            if ($result->item(0)->nodeValue == 'true') {
-                return true;
-            } else {
-                return false;
-            }
-        } else {
-            return null;
-        }
-    }
+	/**
+	 *
+	 * @return boolean|null
+	 */
+	public function getSeized() {
+    $result = $this->getElementsByTagNameNS('http://www.eurid.eu/xml/epp/domain-ext-2.3', 'seized');
+		if ($result->length > 0) {
+			if ($result->item(0)->nodeValue == 'true') {
+				return true;
+			} else {
+				return false;
+			}
+		} else {
+			return null;
+		}
+	}
 
-    /**
-     *
-     * @return string|null
-     */
-    public function getAvailableDate() {
-        $xpath = $this->xPath();
-        $result = $xpath->query('/epp:epp/epp:response/epp:extension/eurid:ext/eurid:infData/eurid:domain/eurid:availableDate');
-        if ($result->length > 0) {
-            return (Date("Y-m-d",strtotime($result->item(0)->nodeValue)));
-        } else {
-            return null;
-        }
-    }
+	/**
+	 *
+	 * @return string|null
+	 */
+	public function getAvailableDate() {
+		return $this->getDeletionDate();
+	}
 
-    /**
-     *
-     * @return string|null
-     */
-    public function getDeletionDate() {
-        $result = $this->getElementsByTagNameNS('urn:ietf:params:xml:ns:domain-1.0', 'exDate');
-        if ($result->length > 0) {
-            return (date("Y-m-d",strtotime($result->item(0)->nodeValue)));
-        } else {
-            return null;
-        }
-    }
+	/**
+	 *
+	 * @return string|null
+	 */
+	public function getDeletionDate() {
+		$result = $this->getElementsByTagNameNS('urn:ietf:params:xml:ns:domain-1.0', 'exDate');
+		if ($result->length > 0) {
+			return (date("Y-m-d", strtotime($result->item(0)->nodeValue)));
+		} else {
+			return null;
+		}
+	}
 }
-
